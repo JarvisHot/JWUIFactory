d7e1ea2dbdda26d57de8efb767a1ccd904d3daea
jarvis🚴 <objectclass@163.com>
2019-11-07 11:33:12 +0800

    1.0.0

--- JWUIFactory/ViewController.m
+++ JWUIFactory/ViewController.m
@@ -24,7 +24,7 @@
        make.right.equalTo(self.view).with.offset(-20);
        make.height.mas_equalTo(60);
     });
-    UIButtonMakeChain(20, UIButtonTypeClose).frame(CGRectMake(30, 200, 100, 30)).imageDirection(JWButtonImageDirectionTop, 30).text(@"点我啊", UIControlStateNormal).addToSuperView(self.view).click(UIControlEventTouchUpInside, ^(id  _Nonnull sender) {
+    UIButtonMakeChain(20, UIButtonTypeCustom).frame(CGRectMake(30, 200, 100, 30)).text(@"点我啊", UIControlStateNormal).textColor([UIColor redColor], UIControlStateNormal).addToSuperView(self.view).click(UIControlEventTouchUpInside, ^(id  _Nonnull sender) {
         NSLog(@"sender-----%@",sender);
     });
     UIImageView.create(11).animationImages(@[[UIImage imageNamed:@"timg0"],[UIImage imageNamed:@"timg1"],[UIImage imageNamed:@"timg2"],[UIImage imageNamed:@"timg3"],[UIImage imageNamed:@"timg4"],[UIImage imageNamed:@"timg5"],[UIImage imageNamed:@"timg6"]]).animationDuration(1).animationRepeatCount(0).frame(CGRectMake(20, 250, 100, 50)).addToSuperView(self.view).backgroundColor([UIColor redColor]).startAnimating();
