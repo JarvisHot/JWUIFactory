2e1df0f654a610ae56a61cfa4d0a1dea70bf4b46
jarvis🚴 <objectclass@163.com>
2019-11-07 17:47:04 +0800

    添加用例

--- JWUIFactory/ViewController.m
+++ JWUIFactory/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "JWUIFactory.h"
 #import <Masonry/Masonry.h>
-@interface ViewController ()
+@interface ViewController ()<UIScrollViewDelegate>
 
 @end
 
@@ -24,14 +24,20 @@
        make.right.equalTo(self.view).with.offset(-20);
        make.height.mas_equalTo(60);
     });
-    UIButtonMakeChain(20, UIButtonTypeCustom).frame(CGRectMake(30, 200, 100, 30)).text(@"点我啊", UIControlStateNormal).textColor([UIColor redColor], UIControlStateNormal).addToSuperView(self.view).click(UIControlEventTouchUpInside, ^(id  _Nonnull sender) {
+    UIButtonMakeChain(2, UIButtonTypeCustom).frame(CGRectMake(30, 200, 100, 30)).text(@"点我啊", UIControlStateNormal).textColor([UIColor redColor], UIControlStateNormal).addToSuperView(self.view).click(UIControlEventTouchUpInside, ^(id  _Nonnull sender) {
         NSLog(@"sender-----%@",sender);
     });
     UIImageView.create(11).animationImages(@[[UIImage imageNamed:@"timg0"],[UIImage imageNamed:@"timg1"],[UIImage imageNamed:@"timg2"],[UIImage imageNamed:@"timg3"],[UIImage imageNamed:@"timg4"],[UIImage imageNamed:@"timg5"],[UIImage imageNamed:@"timg6"]]).animationDuration(1).animationRepeatCount(0).frame(CGRectMake(20, 250, 100, 50)).addToSuperView(self.view).backgroundColor([UIColor redColor]).startAnimating();
-    
+    UILabel.create(12).frame(CGRectMake(0, 0, 10, 10)).text(@"我是label").textColor(UIColor.yellowColor).font([UIFont systemFontOfSize:30]).addToSuperView(self.view);
+    UITextField.create(13).frame(CGRectMake(20, 310, 100, 20)).placeholder(@"我是占位符").textColor(UIColor.redColor).eventBlock(UIControlEventEditingChanged, ^(UITextField*  _Nonnull sender) {
+        NSLog(@"textfield.text----%@",sender.text);
+    }).addToSuperView(self.view);
+    UIScrollView.create(14).frame(CGRectMake(0, 340, 300, 200)).contentSize(CGSizeMake(0, 300)).addToSuperView(self.view).delegate(self).backgroundColor(UIColor.yellowColor);
     
     // Do any additional setup after loading the view.
 }
-
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    NSLog(@"scroll------%f",scrollView.contentOffset.y);
+}
 
 @end
